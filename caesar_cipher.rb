a8f855019e8968d95e892d9921291c510d9fcf7a
Mystic-Mish <mumiaktech@gmail.com>
2022-12-26 18:50:21 +0300

    Caesar Cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,26 @@
+def caesar_cipher(text, shift)
+  for i in 0...text.length do
+    char_code = text[i].ord
+
+    (a, z) = case char_code
+             when 97..122 then [97, 122]   # a-z
+             when 65..90  then [65, 90]    # A-Z
+             else next
+    end
+
+    rotate = shift > 0 ? 26 : -26
+
+    char_code += shift
+    char_code -= rotate unless char_code.between?(a, z)
+
+    text[i] = char_code.chr
+  end
+end
+
+info = "Ruby is fun."
+
+caesar_cipher(info, 21)
+
+puts info
+
+
